224e54868d4938155620760549a1e271f4ef8889
chaesunbak <coanstjd0422@gmail.com>
2024-10-10 20:49:13 +0900

    주석 추가 및 query()에서 execute()로 변경

--- controller/UserController.js
+++ controller/UserController.js
@@ -3,7 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 
 export async function getAllUsers(req, res) {
   try {
-    const [rows, fields] = await pool.query('SELECT * FROM users');
+    const [rows, fields] = await pool.execute('SELECT * FROM users'); // conn는 그냥 쓰면 되는데 pool은 await pool임, query 대신 execute로 변경
 
     if (rows.length === 0) {
       return response.status(StatusCodes.NOT_FOUND).send('No users found');
