c951d360c438eb75e646049c5be710c90e293166
Chaesunbak <coanstjd0422@gmail.com>
2024-10-10 19:34:00 +0900

    require에서 Import로 리팩토링

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 
-const dotenv = require("dotenv");
+import dotenv from 'dotenv';
 dotenv.config();
 
 // 포트번호를 환경변수로 설정해주세요.
