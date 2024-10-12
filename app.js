3139d74401214d8709764d373b46bf4062a2ebae
Chaesunbak <coanstjd0422@gmail.com>
2024-10-12 23:25:56 +0900

    chore : app.js 에서 json과 cookie-parser모듈 사

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import userRouter from './routes/users.js';
 
 dotenv.config();
 
 const app = express();
 
+// 미들웨어 설정
+app.use(express.json());
+app.use(cookieParser()); // 쿠키 파서 미들웨어 추가
+
 // 포트번호를 환경변수로 설정해주세요.
-const PORT_NUMBER = process.env.PORT_NUMBER;
+const PORT_NUMBER = process.env.PORT_NUMBER || 7777;
 
 app.use('/users', userRouter);
 
