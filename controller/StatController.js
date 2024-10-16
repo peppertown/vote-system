5713310113e6d4c1f3602ff8379e93b2189fb61e
Dong Hyun <decidere@naver.com>
2024-10-16 17:36:23 +0900

    feat : 투표 결과 1등 확인 기능 구현

--- controller/StatController.js
+++ controller/StatController.js
@@ -1,7 +1,46 @@
 import pool from '../mariadb.js';
 
 const showResult = async (req, res) => {
-  res.json('hello world');
+  const surveyId = req.params.id;
+
+  let surveyTitle, totalVoteCount, mostChoiced;
+  let mostChoicedNames = [];
+
+  [surveyTitle] = await pool.execute(`SELECT title FROM surveys WHERE id = ?`, [
+    surveyId,
+  ]);
+
+  [totalVoteCount] = await pool.execute(
+    `SELECT count(*) FROM responses WHERE survey_id = ?`,
+    [surveyId],
+  );
+
+  [mostChoiced] = await pool.execute(
+    `SELECT option_id, COUNT(*) as count FROM answer_choices
+       WHERE response_id IN (SELECT id FROM responses WHERE survey_id = ?)
+       GROUP BY option_id HAVING count = (
+       SELECT MAX(count) FROM ( SELECT COUNT(*) as count FROM answer_choices
+       WHERE response_id IN (SELECT id FROM responses WHERE survey_id = ?)
+       GROUP BY option_id ) as max_counts );`,
+    [surveyId, surveyId],
+  );
+
+  for (let answer of mostChoiced) {
+    let [result] = await pool.execute(
+      'SELECT option_text FROM question_options WHERE id = ?',
+      [answer.option_id],
+    );
+    mostChoicedNames.push(result[0].option_text);
+  }
+
+  let result = {
+    title: surveyTitle[0].title,
+    totalVoteCount: totalVoteCount[0]['count(*)'],
+    mostChoiced: mostChoicedNames,
+    voteCount: mostChoiced[0].count,
+  };
+
+  return res.json(result);
 };
 
 export default showResult;
