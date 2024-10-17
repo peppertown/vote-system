ea8aacb7b34c3125bd6a245d17e9cdb35acf7e84
Chaesunbak <coanstjd0422@gmail.com>
2024-10-17 17:47:30 +0900

    chore : 함수이름 오타 수정

--- routes/questions.js
+++ routes/questions.js
@@ -14,7 +14,7 @@ router.get('/:id/questions', allQuestion); // 질문 조회
 router.patch('/:id/questions/:question_id', editQuestion); // 질문 수정
 router.patch('/:id/questions/:question_id/options/:option_id', editOptions); // 질문 옵션 수정
 router.delete('/:id/questions', deleteQuestion); // 질문 삭제
-router.delete('/:id/questions/:question_id/options/:option_id', editOptions); // 질문 옵션 삭제
+router.delete('/:id/questions/:question_id/options/:option_id', deleteOptions); // 질문 옵션 삭제
 
 // 모듈화
 export default router;
